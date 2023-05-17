f0529215a6bda228d3e3f6e2abf8cbbab6a49ce0
srt1017 <omarfarouq107@gmail.com>
2023-05-17 18:25:01 -0500

    New react app to learn building/managing forms

---
+++ reactforms/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Form from "./components/Form";
+
+function App() {
+  
+
+  return (
+   <div>
+    <Form></Form>
+   </div>
+  )
+}
+
+export default App
