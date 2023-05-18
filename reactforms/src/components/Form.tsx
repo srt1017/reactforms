97e4e31941d7f2bc1cd7ab8a8f9ef88c14a29b79
srt1017 <omarfarouq107@gmail.com>
2023-05-18 14:11:23 -0500

    Added validation functionality

--- reactforms/src/components/Form.tsx
+++ reactforms/src/components/Form.tsx
@@ -1,30 +1,44 @@
-import React, { FormEvent, useRef } from "react";
+import React, { FormEvent, useRef, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+
+interface FormData {
+    name: string,
+    age: number
+}
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: "", age: 0 };
-
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    if (nameRef.current != null) person.name = nameRef.current.value;
-    if (ageRef.current != null) person.age = parseInt(ageRef.current.value);
-    console.log(person);
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+  const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input
+          {...register("name", { required: true, minLength: 3 })}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+        {errors.name?.type === "required" && <p className="text-danger">The name field is required.</p>}
+        {errors.name?.type === "minLength" && (<p className="text-danger">The name must be more than 3 characters.</p>)}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input
+          {...register("age")}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
